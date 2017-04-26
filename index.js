5c6bd9246aeb6e396d3bc13e8dd5b47eecb9ffef
Luke Edwards <luke.edwards05@gmail.com>
2017-04-26 14:54:43 -0700

    add module logic & tests

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
+const parse = require('url').parse;
+
+module.exports = (url, opts) => {
+	if (typeof url !== 'string') {
+		throw new TypeError(`Expected a string, got ${typeof url}`);
 	}
 
 	opts = opts || {};
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	const arr = parse(url).hostname.split('.').reverse();
+
+	if (arr.length === 2) {
+		arr.push('www');
+	}
+
+	if (opts.name) {
+		arr[2] = opts.name;
+	}
+
+	return arr.join(opts.glue || '.');
 };
--- test/index.js
+++ test/index.js
@@ -1,6 +1,41 @@
 const test = require('tape');
 const fn = require('../');
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+const foo = 'https://example.com';
+const bar = 'http://bar.example.com';
+const baz = 'http://example.com/abc/123';
+
+test('must be string', t => {
+	t.plan(1);
+	t.throws(fn, /TypeError/);
+});
+
+test('basic: default `www`', t => {
+	t.equal(fn(foo), 'com.example.www');
+	t.end();
+});
+
+test('basic: subdomain', t => {
+	t.equal(fn(bar), 'com.example.bar');
+	t.end();
+});
+
+test('basic: ignore pathnames', t => {
+	t.equal(fn(baz), 'com.example.www');
+	t.end();
+});
+
+test('options: `name`', t => {
+	t.equal(fn(foo, {name: 'foo'}), 'com.example.foo');
+	t.end();
+});
+
+test('options: `name` overwrite', t => {
+	t.equal(fn(bar, {name: 'hello'}), 'com.example.hello');
+	t.end();
+});
+
+test('options: `glue`', t => {
+	t.equal(fn(foo, {glue: '-'}), 'com-example-www');
+	t.end();
 });
